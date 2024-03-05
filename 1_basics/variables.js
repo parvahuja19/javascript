248c55a3bf5b5be7eb9b8cbe2f7b2abcef7e603b
parvahuja19 <97192395+parvahuja19@users.noreply.github.com>
2024-03-05 14:32:09 +0000

    variables in javascript

---
+++ 1_basics/variables.js
@@ -0,0 +1,18 @@
+//there are 4 types of variable declaration method in javascript
+
+//cannot be changed
+const id=1
+
+//can be changed
+let name="parv ahuja"
+var password="parvprogrammes"
+location="bhubaneshwar"
+
+console.log(id)
+console.log(name)
+console.log(password)
+console.log(location)
+
+console.log("OR")
+
+console.table([id,name,password,location])
